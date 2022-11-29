43a89f35ec5543de87348c6b933b00ab3cf0d9bc
jenna-lab <jennifer.sammy@student.moringaschool.com>
2022-11-29 12:01:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(driver, string) {
+  return driver.filter(function (driver) {
+    return driver.toLowerCase() === string.toLowerCase();
+  });
+}
+function fuzzyMatch(driver, string) {
+  return driver.filter(function (driver) {
+    return (
+      driver.toLowerCase().substring(0, string.length) === string.toLowerCase()
+    );
+  });
+}
+function matchName(drivers, argument) {
+  return drivers.filter(function (driver) {
+    return driver.name === argument;
+  });
+}
